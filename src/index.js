3201e9099800daae0eb280d3c63a2db5589ddd19
Fritz (Fredrick Seitz) <fredrick.seitz@gmail.com>
2019-10-09 15:59:17 -0400

    WIP: getting increment updates on new faas functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const EventService  = require('./event.service');
 const fetch = require('node-fetch');
 const _ = require('lodash');
+const cron = require("node-cron");
 const dotenv = require('dotenv').config({path: __dirname + '/.env'});
 
 const kafkaUri = process.env.KAFKA_CONNECTION;
 const https = process.env.FAAS_SSL === "true";
 const protocol = https ? "https" : "http";
-const faas = `{protocol}://${process.env.FAAS_USER}:${process.env.FAAS_PASS}@${process.env.FAAS_URI}/function/`;
+const faas = `${protocol}://${process.env.FAAS_USER}:${process.env.FAAS_PASS}@${process.env.FAAS_URI}`;
 
 (async () =>
 {
     try {
         let res =
-            await fetch(`https://${process.env.FAAS_USER}:${process.env.FAAS_PASS}@${process.env.FAAS_URI}/system/functions`);
+            await fetch(`${faas}/system/functions`);
         let functions = await res.json();
         //console.log(functions);
         const eventService = new EventService(process.env.CONNECTOR_NAME,
@@ -29,9 +30,9 @@ const faas = `{protocol}://${process.env.FAAS_USER}:${process.env.FAAS_PASS}@${p
             for (let topic of topics) {
                 let f = _.filter(functions, ['annotations.topic', topic]);
                 if(f.length === 0){ continue; }
-                eventService.subscribe(process.env.TOPICS,
-                    `${process.env.CONNECTOR_NAME}-${process.env.TOPICS}`, f, async (payload, done) => {
-                        let r = await fetch(`${faas}${payload.data.metadata.function}`, {
+                eventService.subscribe(topic,
+                    `${process.env.CONNECTOR_NAME}-${topic}`, f, async (payload, done) => {
+                        let r = await fetch(`${faas}/function/${payload.data.metadata.function}`, {
                             method: 'post',
                             body: JSON.stringify(payload.data),
                             headers: {'Content-Type': 'application/json'},
@@ -41,6 +42,17 @@ const faas = `{protocol}://${process.env.FAAS_USER}:${process.env.FAAS_PASS}@${p
             }
 
             await eventService.start();
+            cron.schedule("* * * * *", async function() {
+                let res =
+                    await fetch(`${faas}/system/functions`);
+                let functions = await res.json();
+                let topics = process.env.TOPICS.split(",");
+                for(let topic of topics){
+                    console.log(eventService.subscriptions.get(topic));
+                }
+
+                console.log("subscription updated");
+            });
         }
         else{
             console.error("No topics have been defined, please set env setting TOPICS with a comma delimited string")
